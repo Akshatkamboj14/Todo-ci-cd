a04c9bbc72caa66dabb63a49103887ac7f7ae830
AKSHAT KAMBOJ <akukamboj@gmail.com>
2025-08-14 14:45:58 +0530

    Update TaskManagerApplicationTests.java

--- backend/task-manager/src/test/java/com/example/task_manager/TaskManagerApplicationTests.java
+++ backend/task-manager/src/test/java/com/example/task_manager/TaskManagerApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.task_manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TaskManagerApplicationTests {
