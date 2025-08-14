d2ba086d8d2092608e8d12fb67309cbd2095244b
AKSHAT KAMBOJ <akukamboj@gmail.com>
2025-08-14 15:06:16 +0530

    Update TaskManagerApplicationTests.java

--- backend/task-manager/src/test/java/com/example/task_manager/TaskManagerApplicationTests.java
+++ backend/task-manager/src/test/java/com/example/task_manager/TaskManagerApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.task_manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TaskManagerApplicationTests {
@@ -10,20 +9,5 @@ class TaskManagerApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
-	@Test
-    void dummyTestToCheckAddition() {
-        assertEquals(4, 2 + 2);
-    }
-
-    @Test
-    void dummyTestToCheckStringConcat() {
-        assertEquals("HelloWorld", "Hello" + "World");
-    }
-
-    @Test
-    void dummyTestToCheckBoolean() {
-        assertEquals(true, 10 > 5);
-    }
 
 }
